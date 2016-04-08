9a27933fcc77b7f79c1b31d9d3566accfbfd0ae1
ljore@ukr.net <creative1>
2016-04-08 20:57:36 +0300

    changes with accordance to code checker

--- gamification/src/main/java/juja/microservices/gamification/model/entity/User.java
+++ gamification/src/main/java/juja/microservices/gamification/model/entity/User.java
@@ -3,7 +3,6 @@ package juja.microservices.gamification.model.entity;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.PersistenceConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 import java.util.UUID;
 
 @Document(collection = "users")
