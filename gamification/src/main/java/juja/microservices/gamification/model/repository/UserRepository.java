ed5a3acea67c24e5dd8184d7a6b8aba1de6cea89
ljore@ukr.net <creative1>
2016-04-21 20:35:50 +0300

    fixed according qualice comments

--- gamification/src/main/java/juja/microservices/gamification/model/repository/UserRepository.java
+++ gamification/src/main/java/juja/microservices/gamification/model/repository/UserRepository.java
@@ -1,14 +1,65 @@
+/**
+ * Copyright (c) 2016, juja.com.ua
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * * Neither the name of microservices nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package juja.microservices.gamification.model.repository;
 
+import java.util.List;
 import juja.microservices.gamification.model.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
 
-public interface UserRepository extends MongoRepository<User, String>, UserRepositoryCustom{
+/**
+ * User repository interface
+ * @author Sergii Lisnychyi (ljore@ukr.net)
+ * @version $Id$
+ * @since 1.0
+ */
+public interface UserRepository extends MongoRepository<User, String>, UserRepositoryCustom {
 
+    /**
+     * Create user.
+     * @param username username
+     * @return Info about created user
+     */
     String createUser(String username);
 
+    /**
+     * Get user by uuid.
+     * @param uuid uuid
+     * @return User
+     */
     User getUser(String uuid);
 
+    /**
+     * Get list of all users.
+     * @return List of users
+     */
     List<User> getUsers();
 }
